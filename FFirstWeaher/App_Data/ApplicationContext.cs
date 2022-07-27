9e065437db4bf8365a7a5e84b85ef6fee843fc23
EmilStar <starostin.emil@mail.ru>
2022-07-27 16:18:15 +0300

    w

--- FFirstWeaher/App_Data/ApplicationContext.cs
+++ FFirstWeaher/App_Data/ApplicationContext.cs
@@ -9,14 +9,10 @@ namespace FFirstWeaher.App_Data
         {
             Database.EnsureCreated();
         }
-        public ApplicationContext(DbContextOptions<ApplicationContext> options)
-            : base(options)
-        {
-            Database.EnsureCreated();   // создаем базу данных при первом обращении
-        }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder opBild)
         {
-            opBild.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database = Weatherdb; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False;");
+            opBild.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database = Weatherdb; Initial Catalog = WeatherBD; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False;");
         }
     }
 }
--- FFirstWeaher/Controllers/AddExController.cs
+++ FFirstWeaher/Controllers/AddExController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace FFirstWeaher.Controllers
 {
@@ -8,5 +11,23 @@ namespace FFirstWeaher.Controllers
         {
             return View();
         }
+        /*public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(fileStream);
+                }
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+                _context.Files.Add(file);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }*/
     }
 }
